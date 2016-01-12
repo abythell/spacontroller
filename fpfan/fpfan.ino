5ea2de5394b56f92ac964929bb064b7f3e9271b9
abythell <abythell@ieee.org>
2016-01-11 21:19:36 -0800

    initial import

---
+++ fpfan/fpfan.ino
@@ -0,0 +1,40 @@
+/**
+ * Fireplace Fan Control
+ * Copyright 2016 Andrew Bythell <abythell@ieee.org>
+ * http://angryelectron.com
+ */
+
+
+/**
+ * Hardware Settings
+ */
+#define SWITCH 2
+#define RELAY 8
+#define FAN_ON  digitalWrite(RELAY, HIGH); digitalWrite(13,HIGH);
+#define FAN_OFF digitalWrite(RELAY, LOW); digitalWrite(13,LOW);
+
+/**
+ * Setup
+ */
+void setup() {
+  Serial.begin(9600);
+  pinMode(SWITCH, INPUT_PULLUP);
+  pinMode(RELAY, OUTPUT);
+  pinMode(13, OUTPUT); /* onboard LED indicates call for fan */
+  FAN_OFF;
+}
+
+/**
+ * Loop
+ */
+void loop() {
+
+  //TODO: use interrupt (on change) and sleep
+  if (digitalRead(SWITCH)) {
+    FAN_OFF;
+  } else {
+    FAN_ON;
+  }  
+  delay(1000);
+}
+
